83a00c5b2935db7c93ac86c50230cd9897efbf6f
EdwinDij <edwin.d899@gmail.com>
2022-10-29 21:40:16 +0200

    construction du fichier obj

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles/App.scss';
-
+import { dataRestaurant } from './data';
 function App() {
+
+  console.log(dataRestaurant)
+  
   return (
     <div className="App">
-
+      bonjour
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const dataRestaurant = [
+    {
+        Ménilmontant:
+        {
+            name: "Palette du goût",
+            city: "Ménilmontant",
+        },
+    }, {
+        Charonne:
+        {
+            name: "La note enchantée",
+            city: 'Charonne',
+        },
+    }, {
+        Cité_Route:
+        {
+            name: "À la française",
+            city: 'Cité Rouge',
+            starter: [{
+               name: ['Tartare de pouple acidulé', "Vélouté de légumes", 'Soupe à l\'oignon'],
+               description: ['Aux zestes d\'orange', 'Carotte, panais, topinambour', 'Revisité'],
+               prix: ['25€', '35€' , '20€' ],
+            }],
+            dish: [{
+                name: ['']
+            }]
+        },
+    }, {
+        Folie_Méricourt:
+        {
+            name: "Le délice des sens",
+            city: "Folie-Méricourt",
+        }
+    },
+]
